887fd196c3d3917da9b34d13b14e93df82aa41b6
eduardofrohlich <eduardofrohlich121@gmail.com>
2024-01-07 17:34:04 -0300

    places list component
    image list to my places screen

--- project_great_places/lib/screens/places_list_screen.dart
+++ project_great_places/lib/screens/places_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:project_great_places/providers/great_places.dart';
 import 'package:project_great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
 class PlacesListScreen extends StatelessWidget {
   @override
@@ -16,8 +18,23 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: CircularProgressIndicator(),
+      body: Consumer<GreatPlaces>(
+        child: Center(
+          child: Text('No places yet!'),
+        ),
+        builder: (ctx, greatPlaces, ch) => greatPlaces.itemsCount == 0
+            ? ch!
+            : ListView.builder(
+                itemCount: greatPlaces.itemsCount,
+                itemBuilder: (context, index) => ListTile(
+                  leading: CircleAvatar(
+                    backgroundImage:
+                        FileImage(greatPlaces.findItemByIndex(index).image),
+                  ),
+                  title: Text(greatPlaces.findItemByIndex(index).title),
+                  onTap: () {},
+                ),
+              ),
       ),
     );
   }
